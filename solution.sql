7961dc5224f8a73a41bc7e972606f896b520ddfd
JeremyCloutier <jeremycloutier@Jeremys-MacBook-Pro.local>
2016-01-19 15:34:29 -0600

    Fourth commit, improves specificity and syntax of answers.

--- solution.sql
+++ solution.sql
@@ -3,19 +3,22 @@
 --Write the SQL query that answers the question, below that comment.
 
 --1.Which authors are represented in our store?
-SELECT * FROM authors;
+SELECT last_name, first_name FROM authors;
 
 --2. Which authors are also distinguished authors?
-SELECT * FROM distinguished_authors;
+SELECT last_name, first_name FROM distinguished_authors;
 
 --3. Which authors are not distinguished authors?
-SELECT * FROM authors LEFT JOIN distinguished_authors ON authors.first_name = distinguished_authors.first_name WHERE award IS null;
+SELECT authors.last_name, authors.first_name
+FROM authors LEFT JOIN distinguished_authors
+ON authors.first_name = distinguished_authors.first_name WHERE award IS null;
 
 --4. How many authors are represented in our store?
 SELECT COUNT(id) FROM authors;
 
 --5. Who are the favorite authors of the employee with the first name of Michael?
-SELECT * FROM favorite_authors;
+SELECT * FROM employees JOIN favorite_authors
+ON employees.id = favorite_authors.employee_id;
 
 --6. What are the titles of all the books that are in stock today?
 SELECT title FROM books JOIN editions
@@ -24,9 +27,9 @@ ON editions.isbn = stock.isbn WHERE stock != 0;
 
 --7. Insert one of your favorite books into the database. Hint: You’ll want to create data in at least 2 other tables to completely create this book.
 INSERT INTO books (id, title, author_id, subject_id)
-    VALUES (12345, 'The Unifying Force', 6789, 2456);
+VALUES (12345, 'The Unifying Force', 6789, 2456);
 INSERT INTO book_backup (id, title, author_id, subject_id)
-    VALUES (12345, 'The Unifying Force', 6789, 2456);
+VALUES (12345, 'The Unifying Force', 6789, 2456);
 
 --8. What authors have books that are not in stock?
 SELECT first_name, last_name FROM authors JOIN books
